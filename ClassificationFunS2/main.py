ca45b6bc7112ca08617bc7e322e48c0ca28109a5
Gina Sprint <sprint@gonzaga.edu>
2021-02-25 07:22:09 -0800

    ClassificationFun starter code

---
+++ ClassificationFunS2/main.py
@@ -0,0 +1,21 @@
+
+
+
+def main():
+    header = ["att1", "att2"]
+    train = [
+        [3, 2],
+        [6, 6],
+        [4, 1],
+        [4, 4],
+        [1, 2],
+        [2, 0],
+        [0, 3],
+        [1, 6]
+    ]
+    train_labels = ["no", "yes", "no", "no", "yes", "no", "yes", "yes"]
+    test = [2, 3]
+
+
+if __name__ == "__main__":
+    main()
